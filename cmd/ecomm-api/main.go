726d96e0d4d2653edf6c27e99e7d24469fcda8d3
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-23 20:32:15 -0400

    adding database connection api

--- cmd/ecomm-api/main.go
+++ cmd/ecomm-api/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"github.com/bloodgroup-cplusplus/ecomm/db"
+)
 func main() {
-	db.NewDatabase()
+	db, err := db.NewDatabase()
+	if err != nil {
+		log.Fatal("Error opening database :%v",err)
+	}
+	defer db.Close()
+	log.Prinln("Successfully connected to database")
+
+	// do something with the databawse
 }
+
+
+
 

0a0ae0778e3b4101137b4cff569e2f141af1af2a
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-23 20:28:45 -0400

    adding jmoiron and db.go

---
+++ db/db.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+
+
+type Database struct {
+	db *sqlx.DB
+}
+
+
+
+func NewDatabase() (*Database,error) {
+	db, err := sqlx.Open("mysql","root:password@tcp(localhost:3306)/ecomm?parseTime=true")
+
+	if err !=nil {
+		return nil, fmt.Error("Error opening database, %w",err)
+	}
+
+	return &Database{db:db},nil
+}
+
+
+
+func (d *Database) Close() error {
+	return db.db.Close()
+}
+
+
+func (d *Database) GetDB() *sqlx.DB {
+	return d.db
+}
+
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bloodgroup-cplusplus/ecomm
 
 go 1.23.3
+
+require github.com/jmoiron/sqlx v1.4.0 // indirect
